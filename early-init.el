76e8c92eb32493cc662fbd5d014ea9c3fccc4f13
Linas Vidziunas <linasvidz@gmail.com>
2026-05-08 18:47:27 +0200

    refactor(early-init.el): increase gc-cons-threshold

--- early-init.el
+++ early-init.el
@@ -19,6 +19,9 @@
 ;;
 ;;; Code:
 
+;; Increase garbage collection during init
+(setq gc-cons-threshold most-positive-fixnum)
+
 ;; Disable UI elements
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
